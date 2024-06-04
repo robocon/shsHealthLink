33ed8cce7bbcce4bbb53218581eff4964b6ed163
kritsanasak kuntaros <roboconk@gmail.com>
2024-06-04 11:12:06 +0700

    procedure แยก opd กับ ipd

--- tables/client_procedure.sql
+++ tables/client_procedure.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80017
 File Encoding         : 65001
 
-Date: 2024-05-27 14:31:14
+Date: 2024-06-04 11:11:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,18 +20,18 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `client_procedure`;
 CREATE TABLE `client_procedure` (
-  `source_id` varchar(255) DEFAULT NULL,
-  `id` varchar(255) DEFAULT NULL,
-  `patient_cid` varchar(255) DEFAULT NULL,
-  `encounter_id` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `source_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `patient_cid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `encounter_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `category_display` varchar(255) DEFAULT NULL,
   `category_code` varchar(255) DEFAULT NULL,
-  `code_display` varchar(255) DEFAULT NULL,
-  `code_code` varchar(255) DEFAULT NULL,
-  `code_system` varchar(255) DEFAULT NULL,
-  `performed_date_time` datetime DEFAULT NULL,
-  `performer` varchar(255) DEFAULT NULL,
+  `code_display` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `code_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `code_system` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `performed_date_time` datetime NOT NULL,
+  `performer` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `reason_display` varchar(255) DEFAULT NULL,
   `reason_code` varchar(255) DEFAULT NULL,
   `reason_system` varchar(255) DEFAULT NULL,
@@ -41,8 +41,8 @@ CREATE TABLE `client_procedure` (
   `outcome_code` varchar(255) DEFAULT NULL,
   `note` varchar(255) DEFAULT NULL,
   `note_date_time` datetime DEFAULT NULL,
-  `deleted` tinyint(1) DEFAULT NULL,
-  `last_update_date_time` datetime DEFAULT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  `last_update_date_time` datetime NOT NULL,
   KEY `patient_cid` (`patient_cid`),
   KEY `encounter_id` (`encounter_id`),
   KEY `id` (`id`) USING BTREE
