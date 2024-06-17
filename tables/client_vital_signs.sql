d1bd982c5e0df08d2ebb87031a7d0e808188443b
kritsanasak kuntaros <roboconk@gmail.com>
2024-06-17 16:16:59 +0700

    ปรับ tmp_resulthead, tmp_resultdetail ปรับ เงื่อนไขของ vital_signs

--- tables/client_vital_signs.sql
+++ tables/client_vital_signs.sql
@@ -10,35 +10,39 @@ Target Server Type    : MYSQL
 Target Server Version : 80017
 File Encoding         : 65001
 
-Date: 2024-06-17 11:10:43
+Date: 2024-06-17 16:15:11
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for client_diagnostic_report
+-- Table structure for client_vital_signs
 -- ----------------------------
-DROP TABLE IF EXISTS `client_diagnostic_report`;
-CREATE TABLE `client_diagnostic_report` (
+DROP TABLE IF EXISTS `client_vital_signs`;
+CREATE TABLE `client_vital_signs` (
   `source_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `patient_cid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `encounter_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `category_display` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `category_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `category_system` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `code_display` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `code_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `code_system` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `effective_date_time` varchar(255) NOT NULL,
   `effective_period_start` varchar(255) DEFAULT NULL,
   `effective_period_end` varchar(255) DEFAULT NULL,
   `performer` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `results_interpreter` varchar(255) DEFAULT NULL,
-  `conclusion` varchar(255) DEFAULT NULL,
-  `conclusion_display` varchar(255) DEFAULT NULL,
-  `conclusion_code` varchar(255) DEFAULT NULL,
+  `value_string` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `value_qty` varchar(10) NOT NULL,
+  `value_unit` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `value_code` varchar(255) DEFAULT NULL,
+  `value_system` varchar(255) DEFAULT NULL,
+  `interpretation_display` varchar(255) DEFAULT NULL,
+  `interpretation_code` varchar(255) DEFAULT NULL,
+  `reference_range_low` varchar(10) DEFAULT NULL,
+  `reference_range_high` varchar(10) DEFAULT NULL,
+  `reference_range_text` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `note_date_time` varchar(255) DEFAULT NULL,
   `deleted` tinyint(4) NOT NULL,
   `last_update_date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
